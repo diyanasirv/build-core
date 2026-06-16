553280e04237d3869120f228cc6b5f6f79525bb8
Diya Nasir V <135638889+diyanasirv@users.noreply.github.com>
2026-06-16 20:12:14 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const supabaseUrl = "https://xrlbrdtatntsbharthpw.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhybGJyZHRhdG50c2JoYXJ0aHB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NjQ5MTksImV4cCI6MjA4NDM0MDkxOX0.X5hA0kLq1gwqgo_17AvisPmq6LDfHmlvTnfhUwFinLc";
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
